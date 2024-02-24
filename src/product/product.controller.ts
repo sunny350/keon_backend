81865647ab05423f1726edc7838270f0ab09c109
Sunny Ramani <sunnyramani201311@gmail.com>
2024-02-24 15:18:45 +0530

    get product by category api added

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,10 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { getCategoryDto } from 'src/category/DTO/get_category.dto';
 
 @Controller('product')
 export class ProductController {
     constructor(
         private readonly productService: ProductService,
     ){}
+
+    @Post('fetchProductsByCategory')
+    async fetchProductsByCategory(@Body() body:getCategoryDto){
+        try {
+            const {category_id} = body
+            let products = await this.productService.getProductsByCategory(category_id)
+            return {
+                success : true,
+                data : products
+            }
+            
+        } catch (error) {
+            return {
+                success : false,
+                message : "unable to get products ",
+                error : error.message || error,
+            }
+        }
+    }
 }
                 
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -8,4 +8,48 @@ export class ProductService {
     constructor(
         @InjectModel("products") private products: Model<ProductDocument>,
     ){}
+
+    async getProductsByCategory(category_id){
+        return await this.products.aggregate([
+            {
+              $match: {
+                categories_id: {
+                  $in: [
+                    category_id
+                  ]
+                }
+              }
+            }, 
+            {
+              $group: {
+                _id: '$sku', 
+                categories_id: {
+                  $first: '$categories_id'
+                }, 
+                product_name: {
+                  $first: '$product_name'
+                }, 
+                discount_percent: {
+                  $first: '$discount_percent'
+                }, 
+                images: {
+                  $first: '$images'
+                }, 
+                primary_image: {
+                  $first: '$primary_image'
+                }, 
+                price: {
+                  $first: '$price'
+                }, 
+                sizes: {
+                  $push: {
+                    size: '$size', 
+                    qty: '$qty', 
+                    id: '$_id'
+                  }
+                }
+              }
+            }
+          ])
+    }
 }
